b7e91b29e7b047fe7a0c542acdb9ca7217a88c85
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-07-23 02:09:43 +0900

    インプットした内容をログに表示

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 console.log("App.js: loaded");
 
 export class App {
-  constructor() {
-    console.log("App initialized");
+  mount() {
+    // form
+    const formElement = document.querySelector("#js-form");
+
+    // input
+    const inputElement = document.querySelector("#js-form-input");
+
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log(`入力欄の値：${inputElement.value}`);
+    });
   }
 }
